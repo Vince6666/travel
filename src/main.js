13fe8570f33eff4918ddb09bacac7cb1ae037de0
Vince <13257688007@163.com>
2019-03-08 15:18:05 +0800

    project code init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'   // 解决移动端 '1像素边框' 的问题
 
 Vue.config.productionTip = false
+fastClick.attach(document.body);
 
 /* eslint-disable no-new */
 new Vue({
